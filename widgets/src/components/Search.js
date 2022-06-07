e99ace29b7208c9ece1987c5a76246aea581beb3
Nenad <68515352+Nenad95M@users.noreply.github.com>
2022-06-07 14:12:41 +0200

    a

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,7 +1,14 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 
 const Search=()=>{
     const [term, setTerm]=useState('');
+    useEffect(()=>{
+       const search=async ()=>{
+           await axios.get('https://en.wikipedia.org/w/api.php');
+       }
+       search();
+    }, [term])
     return (<div>
         <div className="ui form">
             <div className="field">

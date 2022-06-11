8fa6bb301836ade171a060b1f72e17d1ab8fcb5b
Nenad <68515352+Nenad95M@users.noreply.github.com>
2022-06-11 20:20:09 +0200

    a

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,9 +1,10 @@
-import React, {useState} from "react";
-// import Search from "./components/Search";
+import React, { useState } from "react";
+import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
-
-// import Accordion from "./components/Accordion";
+import Accordion from "./components/Accordion";
+import Header from "./components/Header";
+import Route from "./components/Route";
 
 const items = [
     { title: "What is React?", content: "React is front end and javascript framework" },
@@ -22,9 +23,25 @@ const options = [
     { label: "The color blue", value: 'blue' }
 ]
 export default () => {
-
-return (<div>
-       <Translate />
-      
+  const [selected, setSelected]=useState(options[0]);
+    return (<div>
+        <Header />
+        <Route path="/">
+            <Accordion items={items} />
+        </Route>
+        <Route path="/list">
+            <Search  />
+        </Route>
+        <Route path="/dropdown">
+            <Dropdown 
+            label={"Select a color"}
+            options={options}
+            selected={selected}
+            onSelectedChange={setSelected}
+            />
+        </Route>
+        <Route path="/translate">
+            <Translate />
+        </Route>
     </div>);
 }; 

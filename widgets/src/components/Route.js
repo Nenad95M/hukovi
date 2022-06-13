929d3eed7abd3d950e1fe7f188c2ecc923a0ffa1
Nenad <68515352+Nenad95M@users.noreply.github.com>
2022-06-13 08:05:32 +0200

    a

--- widgets/src/components/Route.js
+++ widgets/src/components/Route.js
@@ -1,5 +1,18 @@
+import { useEffect, useState } from "react";
+
 const Route=({path, children})=>{
-    return window.location.pathname===path ? children :null;
+const [currentPath, setCurentPath]= useState(window.location.pathname);
+ useEffect(()=>{
+    const onLocationChange=()=>{
+       setCurentPath(window.location.pathname);
+    };
+    window.addEventListener('popstate', onLocationChange);
+    return ()=>{
+        window.removeEventListener('popstate', onLocationChange);
+    };
+ }, []);
+
+    return currentPath===path ? children :null;
 };
 
 export default Route;
